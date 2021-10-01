2d4f3fc0065057bfdcda6bccb0bfab6a7309980c
jesshuck <jesshuck1@yahoo.co.uk>
2021-10-01 11:51:30 +0100

    adding tests

--- app.test.js
+++ app.test.js
@@ -17,20 +17,34 @@ describe('GET requests', () => {
 
 });
 
-// TEST THE REST API ENDPOINT FOR PUT
+// TEST THE REST API ENDPOINT FOR POST
 describe('CREATE request', () => {
     
     test('CREATE product test', async () => {
 	// TEST IN HERE
+    const res = await request(app).post('/product/create')
+    send({name : "test name", 
+
+        description : "test desc",
+
+        price : 0});
+    expect(res.statusCode).toBe(201)
     });
 
 });
 
+
 // UNIT TEST THE PRODUCT BUILDER
 describe('Unit Tests', () => {
 
+    let obj = {
+        name: "name",
+        description: "desc",
+        price: 15.99
+    };
     test('product object builder', () => {
         // TEST IN HERE
+        expect(productBuilder("name", "desc", 15.99)).toEqual(obj);
     });
 
 });
